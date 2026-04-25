b3b0d1bfd51576cce14b3ff8127170200c8a20d4
Angeltrek <angeltrek007@gmail.com>
2026-04-25 16:41:19 -0600

    feat(Translator): Logic and Void methods added

--- simple-parser/Translator.py
+++ simple-parser/Translator.py
@@ -21,7 +21,7 @@ class Void(Node):
 class Number(Numeric):
     def __init__(self, value):
         self.value = value
-    
+
     def eval(self, env):
         return self.value
 
@@ -35,7 +35,7 @@ class Identifier(Numeric):
         if result != None:
             (_, value) = result
             return value
-        else: 
+        else:
             text = "Line " + str(self.line) + " - " + self.name + " has not been declared"
             raise Exception(text)
 
@@ -44,7 +44,8 @@ class Minus(Numeric):
         self.right = right
 
     def eval(self, env):
-        return -1 * float(self.right.eval(env))
+        right = float(self.right.eval(env))
+        return -1 * right
 
 class Add(Numeric):
     def __init__(self, left, right):
@@ -97,3 +98,154 @@ class Mod(Numeric):
         left = float(self.left.eval(env))
         right = float(self.right.eval(env))
         return left % right
+
+# --- LOGIC --- #
+class TrueVal(Logic):
+    def eval(self, env):
+        return True
+
+class FalseVal(Logic):
+    def eval(self, env):
+        return False
+
+class Not(Logic):
+    def __init__(self, right):
+        self.right = right
+
+    def eval(self, env):
+        right = self.right.eval(env)
+        return not right
+
+class And(Logic):
+    def __init__(self, left, right):
+        self.left = left
+        self.right = right
+
+    def eval(self, env):
+        left = bool(self.left.eval(env))
+        if not left:
+            return False
+        right = bool(self.right.eval(env))
+        return right
+
+class Or(Logic):
+    def __init__(self, left, right):
+        self.left = left
+        self.right = right
+
+    def eval(self, env):
+        left = bool(self.left.eval(env))
+        if left:
+            return True
+        right = bool(self.right.eval(env))
+        return right
+
+class LessThan(Logic):
+    def __init__(self, left, right):
+        self.left = left
+        self.right = right
+
+    def eval(self, env):
+        left = float(self.left.eval(env))
+        right = float(self.right.eval(env))
+        return left < right
+
+class GreaterThan(Logic):
+    def __init__(self, left, right):
+        self.left = left
+        self.right = right
+
+    def eval(self, env):
+        left = float(self.left.eval(env))
+        right = float(self.right.eval(env))
+        return left > right
+
+class LessOrEqual(Logic):
+    def __init__(self, left, right):
+        self.left = left
+        self.right = right
+
+    def eval(self, env):
+        left = float(self.left.eval(env))
+        right = float(self.right.eval(env))
+        return left <= right
+
+class GreaterOrEqual(Logic):
+    def __init__(self, left, right):
+        self.left = left
+        self.right = right
+
+    def eval(self, env):
+        left = float(self.left.eval(env))
+        right = float(self.right.eval(env))
+        return left >= right
+
+class Equal(Logic):
+    def __init__(self, left, right):
+        self.left = left
+        self.right = right
+
+    def eval(self, env):
+        left = self.left.eval(env)
+        right = self.right.eval(env)
+        return left == right
+
+class NEQ(Logic):
+    def __init__(self, left, right):
+        self.left = left
+        self.right = right
+
+    def eval(self, env):
+        left = self.left.eval(env)
+        right = self.right.eval(env)
+        return left != right
+
+# --- VOID (statements) --- #
+class Print(Void):
+    def __init__(self, expression):
+        self.expression = expression
+
+    def eval(self, env):
+        value = self.expression.eval(env)
+        print(value)
+
+class Assignment(Void):
+    def __init__(self, name, expression, line):
+        self.name = name
+        self.expression = expression
+        self.line = line
+
+    def eval(self, env):
+        value = self.expression.eval(env)
+        value_type = Type.BOOLEAN if isinstance(value, bool) else Type.NUMBER
+        if not env.set(self.name, value_type, value):
+            text = "Line " + str(self.line) + " - " + self.name + " has not been declared"
+            raise Exception(text)
+
+class StatementSequence(Void):
+    def __init__(self, statements):
+        self.statements = statements
+
+    def eval(self, env):
+        for stmt in self.statements:
+            stmt.eval(env)
+
+class Declaration(Void):
+    def __init__(self, names, line):
+        self.names = names
+        self.line = line
+
+    def eval(self, env):
+        for name in self.names:
+            if not env.insert(name):
+                text = "Line " + str(self.line) + " - " + name + " has already been declared"
+                raise Exception(text)
+
+class Program(Void):
+    def __init__(self, declaration, statements):
+        self.declaration = declaration
+        self.statements = statements
+
+    def eval(self, env):
+        self.declaration.eval(env)
+        self.statements.eval(env)
